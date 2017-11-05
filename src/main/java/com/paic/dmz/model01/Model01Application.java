63c8571a4e418c6449a0e34465b9d075ccbe8904
banzhuaner <zhenyu_peng@yeah.net>
2017-11-05 13:16:27 +0800

    create project

---
+++ src/main/java/com/paic/dmz/model01/Model01Application.java
@@ -0,0 +1,12 @@
+package com.paic.dmz.model01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Model01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Model01Application.class, args);
+	}
+}
---
+++ src/main/java/com/paic/dmz/model01/UserControoller.java
@@ -0,0 +1,18 @@
+package com.paic.dmz.model01;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class UserControoller {
+
+    @Value("${cupSize}")
+    private  String cupSize;
+    @RequestMapping("/hello")
+    public String sayHi(ModelAndView mav){
+
+        return  "Hello World!"+ cupSize;
+    }
+}
